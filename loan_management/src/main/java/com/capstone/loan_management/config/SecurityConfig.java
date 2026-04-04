a131b4ef923f04c0831f829aeb0744e0eb5940be
aryandeshpande11 <aryan.deshpande29@gmail.com>
2026-04-05 01:44:45 +0530

    fixed bugs

--- loan_management/src/main/java/com/capstone/loan_management/config/SecurityConfig.java
+++ loan_management/src/main/java/com/capstone/loan_management/config/SecurityConfig.java
@@ -33,8 +33,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/api/loans/**").hasRole("USER")
+                        .requestMatchers("/api/loans/**").permitAll()
+                        .requestMatchers("/api/admin/**").permitAll()
                         .anyRequest().authenticated());
 
         return http.build();
